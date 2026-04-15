9b1608a591714ead1b6cf5c17e1906e8043c7565
Quang Pham <quangnp0903@gmail.com>
2026-04-15 18:15:51 +0700

    Fix cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
   'http://localhost:5173',
-  'https://jobify-fullstack-fe.onrender.com/',
+  'https://jobify-fullstack-fe.onrender.com',
 ];
--- frontend/src/utils/customFetch.ts
+++ frontend/src/utils/customFetch.ts
@@ -4,6 +4,7 @@ const API = import.meta.env.VITE_API_URL;
 
 const customFetch = axios.create({
   baseURL: `${API}/v1`,
+  withCredentials: true,
 });
 
 export default customFetch;
